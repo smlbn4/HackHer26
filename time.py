0f9416965b6d2645ffc59e001796f822cf8124bb
smlbn4 <hockeycat4s@gmail.com>
2026-02-21 16:53:19 -0500

    created time.py

---
+++ time.py
@@ -0,0 +1,30 @@
+from stopwatch import Stopwatch
+class Time:
+
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def add_hours(self,hours=1):
+        self.value += hours
+
+    def add_from_timer(self,timer:Stopwatch):
+        self.value += timer.elapsed_time
+        timer.reset()
+
+    def time_spent(self,hours): #helper for buy
+        self.value -= hours
+
+    def can_spend(self,hours_required): #helper for buy
+        if self.value - hours_required >= 0:
+            return True
+        else:
+            return False   
+
+    def buy(self,hours_price): 
+        if self.can_spend(hours_price):
+            self.time_spent(hours_price)
+        else:
+            print("Not enough hours!")
