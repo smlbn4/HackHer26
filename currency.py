268e6339d712f7a1d2290f2dc58739b299b3dca8
Isabella Harrington <isabellaharrington643@gmail.com>
2026-02-21 15:22:41 -0500

    Created currency class + its attributes and methods

---
+++ currency.py
@@ -0,0 +1,18 @@
+class currency:
+    def __init__(self, value):
+        self.value = value
+
+    def increase_value(self, x):
+        self.value += x
+
+    def decrease_value(self, x):
+        self.value -= x
+
+    def get_value(self):
+        return self.value
+    
+    def can_spend(self, amt):
+        if self.value - amt > 0:
+            return True
+        else:
+            return False
