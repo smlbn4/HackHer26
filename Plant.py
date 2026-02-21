1d02e41d7e452fa2a124ddc0f361ef82546d52e0
Isabella Harrington <isabellaharrington643@gmail.com>
2026-02-21 13:47:12 -0500

    Added code to test Plant.py

--- Plant.py
+++ Plant.py
@@ -24,4 +24,6 @@ class Plant:
 
 if __name__ == "__main__":
     flower1 = Plant("Daisy", "demo_img_folder", {"Common": 0.33, "Rare": 0.1}, 3.50)
-    print(flower1.get_img())
+    
+    flower1.water_plant()
+    
