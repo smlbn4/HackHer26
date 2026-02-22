6e4e0971888f48eeb614aa7e4e8529883584fe62
Isabella Harrington <isabellaharrington643@gmail.com>
2026-02-22 02:20:12 -0500

    Added bugCompendium test code

--- bugCompendium.py
+++ bugCompendium.py
@@ -23,9 +23,28 @@ if __name__ == "__main__":
     bumblebee = bug("bumblebee", 0.5, "The bumblebee is a bug.", "beebalm")
     bumblebee.is_found()
     grasshopper = bug("grasshopper", 0.2, "The grasshopper is a bug.", "lettuce")
+    grasshopper.is_found()
+    ladybug = bug("ladybug", 0.4, "The ladybug is a bug.", "geranium")
+    ladybug.is_found()
+    monarch = bug("monarch", 0.3, "The monarch is a bug.", "milkweed")
+    monarch.is_found()
+    mantis = bug("mantis", 0.1, "The mantis is a bug.", "tomato")
+    mantis.is_found()
 
     compendium = bugCompendium()
     print()
+    print(str(compendium.page))
+    print()
+
+    compendium.turn_page_forward()
+    print(str(compendium.page))
+    print()
+
+    compendium.turn_page_forward()
+    print(str(compendium.page))
+    print()
+
+    compendium.turn_page_forward()
     print(str(compendium.page))
     print()
 
