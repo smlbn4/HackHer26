e3c90aec34684c014133b9b50bf6c0b31b7afff9
smlbn4 <hockeycat4s@gmail.com>
2026-02-21 20:54:17 -0500

    bug stuff

--- bug.py
+++ bug.py
@@ -2,11 +2,23 @@ from sprite import sprite
 
 class bug(sprite):
 
+    buglist = []
+
     def __init__(self,probability,spritesfolder):
         self.found = False
         self.probability = probability
         self.spritesfolder = spritesfolder
+
+        bug.buglist.append(self)
+
+    def is_found(self):
+        self.found = True
+
+    def forget(self): #undiscovers bug
+        self.found = False
+        #need to change sprite
+
     def get_prob(self):
         return self.probability
-    def get_sprite(self):
-        return self.spritesfolder #needs to be edited
+    #def get_sprite(self):
+        #return f"{self.spritesfolder}/{self.type}stage{self.stage}.PNG" #needs to be edited
