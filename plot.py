f8456931358003cbf87da103827d6030ecdd23ec
Isabella Harrington <isabellaharrington643@gmail.com>
2026-02-21 20:42:21 -0500

    Created sell_plant() and buy_plant() functions

--- plot.py
+++ plot.py
@@ -3,31 +3,65 @@ import currency
 
 class plot:
     def __init__(self, plot_plant=None):
-        self.isEmpty = True
         self.plot_plant = plot_plant
+        if self.plot_plant != None:
+            self.is_empty = False
+        else:
+            self.is_empty = True
 
     def sell_plant(self, balance:currency):
-        if self.plot_plant.can_sell_plant():
-            plant.plant.plant_types.remove(str(self.plot_plant))
+        if not self.is_empty:
+            if self.plot_plant.can_sell_plant():
+                plant.plant.plant_types.remove(str(self.plot_plant))
 
-            revenue = self.plot_plant.get_sale_price()
-            balance.increase_value(revenue)
+                revenue = self.plot_plant.get_sale_price()
+                balance.increase_value(revenue)
+                self.is_empty = True
+            else:
+                print("Error: Plant is not mature, cannot be sold.")
         else:
-            print("Error: Plant is not mature, cannot be sold.")
+            print("Error: Plot is empty, no plant to be sold.")
+
+    def buy_plant(self, balance:currency, seed:plant):
+        if not self.is_empty:
+            print("Error: Plot is not empty, cannot buy seeds.")
+        else:
+            seed_price = seed.get_purchase_price()
+            if not balance.can_spend(seed_price):
+                print("Error: Insufficient funds.")
+            else:
+                balance.decrease_value(seed_price)
+                self.plot_plant = seed
+                self.is_empty = False
+
+    def watch_plant(self):
+        if not self.is_empty:
+            if self.plot_plant.can_spawn_bugs:
+                pass
+            else:
+                print("Error: Plant cannot spawn bugs.")
+        else:
+            print("Error: Plot is empty, no plant to watch.")
+
 
 
 if __name__ == "__main__":
-    flower1 = plant.plant("PLC", "./Placeholder Sprites/testPlant", sale_price=3.0)
+    flower1 = plant.plant("PLC", "./Placeholder Sprites/testPlant", sale_price=3.0, purchase_price=1.5)
     flower1.water_plant()
     flower1.water_plant()
     flower1.water_plant()
     flower1.water_plant()
 
     balance = currency.currency(0)
-    print(balance, 2)
+    print(balance)
 
     plot1 = plot(flower1)
 
     plot1.sell_plant(balance)
     print(balance)
 
+    plot1.buy_plant(balance, flower1)
+    print(balance)
+
+    plot1.sell_plant(balance)
+    print(balance)
