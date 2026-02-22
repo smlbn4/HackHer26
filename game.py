3cc70dcd7140bc26137439f6addf37690d546b08
smeemp <ejblalock17@gmail.com>
2026-02-22 10:44:03 -0500

    my bad chat

--- game.py
+++ game.py
@@ -128,7 +128,7 @@ def main():
 
         # Draw all sprites
         for s in allSprites:
-            if s.visible and not sw.running:
+            if s.visible:
                 canvas.blit(s.get_image(), s.get_location())
 
         if not sw.running:
